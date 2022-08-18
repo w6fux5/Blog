aded79afb2b3426c81a8e366813744074a367216
w6fux5 <w6fux5@gmail.com>
2022-08-18 18:57:12 +0800

    feat: [frontend] register form and login form

--- client/src/components/auth/Login.tsx
+++ client/src/components/auth/Login.tsx
@@ -1,7 +1,56 @@
-import React from 'react';
+import {
+  Typography, Button, Form, Input,
+} from 'antd';
+import styled from 'styled-components';
+
+import { Link } from 'react-router-dom';
+
+const { Title, Paragraph } = Typography;
+
+const FormBox = styled.div`
+  width: 30vw;
+  background: hsla(0, 0%, 100%, 0.9);
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 export const Login = () => (
-  <div>
-    login
-  </div>
+  <FormBox>
+    <Title>登入</Title>
+    <Form
+      initialValues={{ remember: true }}
+      // onFinish={onFinish}
+      // onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="帳號"
+        name="account"
+        rules={[{ required: true, message: '請輸入帳號!' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="密碼"
+        name="password"
+        rules={[{ required: true, message: '請輸入密碼!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item>
+        <Button block type="primary" htmlType="submit">
+          登入
+        </Button>
+      </Form.Item>
+      <Paragraph>
+        還沒有帳號?
+        <Link to="/register">前往註冊!</Link>
+      </Paragraph>
+    </Form>
+  </FormBox>
 );
--- client/src/pages/AuthPage.tsx
+++ client/src/pages/AuthPage.tsx
@@ -1,7 +1,11 @@
 import { Register } from '../components';
 
 export const AuthPage = () => (
-  <div>
+  <div
+    className="row jc-center al-center"
+    style={{ backgroundColor: 'grey', height: '100vh' }}
+    // style={{ backgroundColor: 'grey', height: '100vh', width: '100vw' }}
+  >
     <Register />
   </div>
 );
