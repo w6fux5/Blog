3688f1172088cdebb2ea6e2e5d5598c5e763ef60
w6fux5 <w6fux5@gmail.com>
2022-08-18 13:47:16 +0800

    chore: [frontend] tailwindcss setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,9 @@
-import reactLogo from './assets/react.svg';
-import './App.css';
-
 const App = () => (
-  <div className="App">
-    <div>
-      <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-        <img src="/vite.svg" className="logo" alt="Vite logo" />
-      </a>
-      <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      </a>
-    </div>
-    <h1>Vite + React</h1>
-    <div className="card">
+  <div className="">
+    <h1 className="text-3xl font-bold underline text-red-600">
+      Hello world!
+    </h1>
 
-      <p>
-        Edit
-        {' '}
-        <code>src/App.tsx</code>
-        {' '}
-        and save to test HMR
-      </p>
-    </div>
-    <p className="read-the-docs">
-      Click on the Vite and React logos to learn more
-    </p>
   </div>
 );
 
